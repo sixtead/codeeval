ebb65ba137c2d20e10b040d06c791c354c82fd07
sixtead <sixtead@gmail.com>
2016-02-28 02:51:26 +0700

    Added solution for maximum primal palindrome

--- codeeval_easy_level_challenge_02_prime_palindrom.rb
+++ codeeval_easy_level_challenge_02_prime_palindrom.rb
@@ -5,13 +5,17 @@ primes << 2
 3.upto 1000 do |number|
   primes_match_count = 0
   primes.each do |prime|
-    # p "#{number} % #{prime} = #{number % prime}"
     if number % prime == 0
-      primes_match_count+=1
+      primes_match_count += 1
       break
     end
   end
   primes << number if primes_match_count == 0
 end
 
-p primes
+max_prime_polindrome = 0
+primes.reverse.each do |prime|
+  max_prime_polindrome = prime if (prime.to_s == prime.to_s.reverse) && prime > max_prime_polindrome
+end
+
+p max_prime_polindrome
