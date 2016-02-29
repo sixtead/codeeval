5557f186076f2e3084f44deee855abf00fc317ef
sixtead <sixtead@gmail.com>
2016-02-29 13:16:25 +0700

    Added solution for challenge N 22 fibonacci series

---
+++ codeeval_022_fibonacci_series.rb
@@ -0,0 +1,15 @@
+fibonacci = [0, 1]
+
+File.open(ARGV[0]).each_line do |line|
+  next if line.chomp.empty?
+  n = line.chomp.to_i
+
+  if fibonacci.length - 1 > n
+    puts fibonacci[n]
+  else
+    (fibonacci.length).upto(n) do |f_number|
+      fibonacci << fibonacci[f_number - 1] + fibonacci[f_number - 2]
+    end
+    puts fibonacci.last
+  end
+end
