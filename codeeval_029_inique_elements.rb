5f7bba6510be6f16914e691b54e506dcb07a0e37
sixtead <sixtead@gmail.com>
2016-03-01 16:19:12 +0700

    Changed solution for challenge N 29

--- codeeval_029_inique_elements.rb
+++ codeeval_029_inique_elements.rb
@@ -1,13 +1,4 @@
 File.open(ARGV[0]).each_line do |line|
   next if line.chomp.empty?
-  numbers = line.chomp.split(',')
-
-  unique_numbers = [numbers.first]
-
-  numbers.each do |number|
-    if number != unique_numbers.last
-      unique_numbers << number
-    end
-  end
-  puts unique_numbers.join(',')
+  puts numbers = line.chomp.split(',').uniq.join(',')
 end
