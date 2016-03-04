e6f6b417d5fb2b74197a1d26b4127cf0f06cca7a
sixtead <sixtead@gmail.com>
2016-03-04 20:02:10 +0700

    Committed because of changes in solution N 29

--- codeeval_022_fibonacci_series.rb
+++ codeeval_022_fibonacci_series.rb
@@ -7,7 +7,7 @@ File.open(ARGV[0]).each_line do |line|
   if fibonacci.length - 1 > n
     puts fibonacci[n]
   else
-    (fibonacci.length).upto(n) do |f_number|
+    fibonacci.length.upto(n) do |f_number|
       fibonacci << fibonacci[f_number - 1] + fibonacci[f_number - 2]
     end
     puts fibonacci.last
