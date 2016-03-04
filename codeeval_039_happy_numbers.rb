273af0901d6b5162b2e1ed316e01e56d2eae3578
sixtead <sixtead@gmail.com>
2016-03-04 20:36:49 +0700

    Completed solution for challenge N 39 happy numbers

--- codeeval_039_happy_numbers.rb
+++ codeeval_039_happy_numbers.rb
@@ -3,20 +3,18 @@ File.open(ARGV[0]).each_line do |line|
   number = line.chomp
   happy_numbers_check = [number]
 
-  while happy_numbers_check.length > 1
-    if happy_numbers_check.last.to_i == 1
-      puts happy_numbers_check.last.to_i
+  while happy_numbers_check.length >= 1
+    break if happy_numbers_check.last.to_i == 1
+    number = 0
+    happy_numbers_check.last.each_char do |chr|
+      number += chr.to_i**2
+    end
+    if happy_numbers_check.include?(number.to_s)
+      happy_numbers_check << 0
       break
     else
-      number = 0
-
-      happy_numbers_check.last.each_char do |chr|
-        number += chr.to_i**2
-      end
-      # TODO: check if number is presented in happy_numbers_check array
-      # if it is there break the loop and puts 0
-
       happy_numbers_check << number.to_s
     end
   end
+  puts happy_numbers_check.last.to_i
 end
