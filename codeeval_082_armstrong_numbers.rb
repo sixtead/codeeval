e124c4f72ec6a0c1ad80545027d915af55d046c6
sixtead <sixtead@gmail.com>
2016-03-06 21:35:30 +0700

    Added solution for challenege N 82 armstrong numbers

---
+++ codeeval_082_armstrong_numbers.rb
@@ -0,0 +1,8 @@
+File.open(ARGV[0]).each_line do |line|
+  next if line.chomp.empty?
+  number = 0
+  line.chomp.each_char do |digit|
+    number += digit.to_i**line.chomp.length
+  end
+  puts number == line.to_i ? "True" : "False"
+end
