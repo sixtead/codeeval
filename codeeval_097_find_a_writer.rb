82ad815d06a755a9ef84b6559b7bb0bbd0892cdd
sixtead <sixtead@gmail.com>
2016-03-10 22:15:31 +0700

    Added solution for challenge N 99 calculate distance

--- codeeval_097_find_a_writer.rb
+++ codeeval_097_find_a_writer.rb
@@ -2,7 +2,7 @@ File.open(ARGV[0]).each_line do |line|
   next if line.chomp.empty?
   crypted_writer_name = line.chomp.split('| ')[0]
   key = line.chomp.split('| ')[1].split(' ')
-  writer_name = String.new
+  writer_name = ''
   key.each do |k|
     writer_name << crypted_writer_name[k.to_i - 1]
   end
