d95bf270aa64215a407dc02a50c91751487e37cb
sixtead <sixtead@gmail.com>
2016-03-10 22:18:36 +0700

    Changed solution for challenge N 99

--- codeeval_099_calculate_distance.rb
+++ codeeval_099_calculate_distance.rb
@@ -1,10 +1,10 @@
 File.open(ARGV[0]).each_line do |line|
   next if line.chomp.empty?
 
-  x1 = line.chomp.split(') (')[0].gsub('(', '').split(', ')[0].to_i
-  y1 = line.chomp.split(') (')[0].gsub('(', '').split(', ')[1].to_i
-  x2 = line.chomp.split(') (')[1].gsub(')', '').split(', ')[0].to_i
-  y2 = line.chomp.split(') (')[1].gsub(')', '').split(', ')[1].to_i
+  x1 = line.chomp.split(') (')[0].delete('(').split(', ')[0].to_i
+  y1 = line.chomp.split(') (')[0].delete('(').split(', ')[1].to_i
+  x2 = line.chomp.split(') (')[1].delete(')').split(', ')[0].to_i
+  y2 = line.chomp.split(') (')[1].delete(')').split(', ')[1].to_i
 
-  puts Math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ).to_i
+  puts Math.sqrt((x2 - x1)**2 + (y2 - y1)**2).to_i
 end
---
+++ codeeval_100_even_numbers.rb
@@ -0,0 +1,5 @@
+File.open(ARGV[0]).each_line do |line|
+  next if line.chomp.empty?
+
+
+end
