d4c377751d6c208bec9afdff1386d6427d1a66e5
sixtead <sixtead@gmail.com>
2016-03-21 21:32:58 +0700

    Solution for 136 racing chars

---
+++ codeeval_136_racing_chars.rb
@@ -0,0 +1,26 @@
+current_location = 0
+File.read(ARGV[0]).each_line.with_index do |line, index|
+  line.chomp.gsub('_', '|') if index == 0
+  if index == 0
+    puts line.chomp.gsub('_', '|')
+    current_location = line.chomp.index('_')
+    next
+  end
+  checkpoint = line.chomp.index('C') || 100
+  gate = line.chomp.index('_') || 100
+  case
+  when checkpoint.between?(current_location - 1, current_location + 1)
+    go_throught = 'C'
+    direction = '/' if checkpoint < current_location
+    direction = '|' if checkpoint == current_location
+    direction = '\\' if checkpoint > current_location
+    current_location = checkpoint
+  when gate.between?(current_location - 1, current_location + 1)
+    go_throught = '_'
+    direction = '/' if gate < current_location
+    direction = '|' if gate == current_location
+    direction = '\\' if gate > current_location
+    current_location = gate
+  end
+  puts line.chomp.gsub(go_throught, direction)
+end
