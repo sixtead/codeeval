5286396e69adc0d2547254dee8f9786c454644c6
sixtead <sixtead@gmail.com>
2016-05-12 21:28:29 +0700

    Changed solution for 227 real fake

--- codeeval_227_real_fake.rb
+++ codeeval_227_real_fake.rb
@@ -1,5 +1,9 @@
 File.read(ARGV[0]).each_line do |line|
-  sum = line.chomp.scan(/\d/).map(&:to_i).each_with_index
-            .inject(0) { |sum, (e, i)| i.odd? ? sum + e : sum + e * 2 }
-  puts "#{sum % 10 == 0 ? 'Real' : 'Fake'}"
+  digits_sum = line.chomp.scan(/\d/).map(&:to_i).each_with_index
+                   .inject(0) { |sum, (e, i)| i.odd? ? sum + e : sum + e * 2 }
+  if digits_sum % 10 == 0
+    puts 'Real'
+  else
+    puts 'Fake'
+  end
 end
